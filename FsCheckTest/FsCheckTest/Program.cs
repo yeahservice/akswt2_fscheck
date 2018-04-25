580636932138ee993c4abad951a8ac18dee3ad83
yeahservice <dom.augustin@gmx.at>
2018-04-25 16:07:57 +0200

    FsCheck example

---
+++ FsCheckTest/FsCheckTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FsCheckTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            Console.WriteLine("2 + 2 = " + calc.add(2, 2));
+            Console.ReadLine();
+        }
+    }
+}
