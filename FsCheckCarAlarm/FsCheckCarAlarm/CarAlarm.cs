72475389373156fa7d69fbdcc9ff7a52dcc344c5
yeahservice <dom.augustin@gmx.at>
2018-04-25 17:02:39 +0200

    einfach alles commitend hat da joe gesagt

---
+++ FsCheckCarAlarm/FsCheckCarAlarm/CarAlarm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FsCheckCarAlarm
+{
+    public enum CarAlarmState
+    {
+        OpenAndUnlocked,
+        ClosedAndUnlocked,
+        OpenAndLocked,
+        ClosedAndLocked
+    }
+
+    public class CarAlarm
+    {
+        private CarAlarmState currentState = CarAlarmState.OpenAndUnlocked;
+
+        public void openCar()
+        {
+            switch (currentState)
+            {
+                case CarAlarmState.ClosedAndUnlocked:
+                    currentState = CarAlarmState.OpenAndUnlocked;
+                    break;
+                case CarAlarmState.ClosedAndLocked:
+                    currentState = CarAlarmState.OpenAndLocked;
+                    break;
+            }
+        }
+
+        public void closeCar()
+        {
+            switch (currentState)
+            {
+                case CarAlarmState.OpenAndUnlocked:
+                    currentState = CarAlarmState.ClosedAndUnlocked;
+                    break;
+                case CarAlarmState.ClosedAndLocked:
+                    currentState = CarAlarmState.ClosedAndLocked;
+                    break;
+            }
+        }
+
+        public void lockCar()
+        {
+            switch (currentState)
+            {
+                case CarAlarmState.OpenAndUnlocked:
+                    currentState = CarAlarmState.OpenAndLocked;
+                    break;
+                case CarAlarmState.ClosedAndUnlocked:
+                    currentState = CarAlarmState.ClosedAndLocked;
+                    break;
+            }
+        }
+
+        public void unlockCar()
+        {
+            switch (currentState)
+            {
+                case CarAlarmState.OpenAndLocked:
+                    currentState = CarAlarmState.OpenAndUnlocked;
+                    break;
+                case CarAlarmState.ClosedAndLocked:
+                    currentState = CarAlarmState.ClosedAndUnlocked;
+                    break;
+            }
+        }
+
+        public void tick()
+        {
+
+        }
+    }
+}
