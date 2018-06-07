2e4d3278db152dc61fd93c1cdf6a8ec4c9e80a56
yeahservice <dom.augustin@gmx.at>
2018-06-07 15:19:59 +0200

    deleted fsharp2 project

--- FsCheckCarAlarm/FsCheckCarAlarm.FSharp2/CarAlarm.fs
+++
@@ -1,9 +0,0 @@
-﻿module CarAlarm =
-
-    type CarAlarm() = 
-        let mutable n = 0
-        member this.Open() = n
-        member this.Close() = n
-        member this.Lock() = n
-        member this.Unlock() = n
-        member this.GetState = n
